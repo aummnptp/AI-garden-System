7a4c0ee6eed8437a49dbbe3f9f8cd2e37233cd73
KittinanPog <64070007@kmitl.ac.th>
2025-02-23 21:21:14 +0700

    fix file name

--- ReactFrontend/src/pages/Project/ProjectPredictAiModelPage.tsx
+++ ReactFrontend/src/pages/Project/ProjectPredictAiModelPage.tsx
@@ -4,7 +4,7 @@ import Sidebar from "../../components/Sidebar";
 import { Alert, AlertTitle,  } from '@mui/material';
 import SkeletonLayout from '../../components/SkeletonPageLayout';
 import { useProjecteData } from '../../hook/projects/useProjectData';
-import { useWorkspaceData } from '../../hook/workspaces/useWorksapceData';
+import { useWorkspaceData } from '../../hook/workspaces/useWorkspaceData';
 import { useAiPrediction } from '../../hook/ai/useAiPrediction';
 import { StepIndicator } from '../../components/ai/predictPage/StepIndicator';
 import { ImageUploadForm } from '../../components/ai/predictPage/ImageUploadForm';
