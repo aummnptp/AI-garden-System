b804f410e41b40026da756c032fc6e1b869bcdb5
Putthipong Chobngam <88504541+aummnptp@users.noreply.github.com>
2025-02-26 23:13:45 +0700

    cookies

--- nest-backend/src/auth/auth.controller.ts
+++ nest-backend/src/auth/auth.controller.ts
@@ -8,49 +8,49 @@ import { Response } from "express";
 
 
 @Controller('auth')
-export class AuthController{
-    constructor(private  readonly authService: AuthService) {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
 
-    @UseGuards(GoogleAuthGuard)
-    @Get("google/login")
-    async googleAuth(@Req() req: Request & { query: any }, @Res() res: Response) {
-      const redirectUrl = (req.query["redirect"] as string) || "/";
-      res.cookie("redirect_after_login", redirectUrl, {
-        httpOnly: true, // ควรเป็น true เพื่อป้องกัน XSS
-        secure: process.env.NODE_ENV === "production", // ควรเป็น true ถ้าใช้ HTTPS
-        domain: "suture-bot.it.kmitl.ac.th", // ไม่ต้องมีจุดนำหน้า
-        sameSite: "none",
-        maxAge: 1000 * 60 * 10, // อายุ 10 นาที
-      });
-  
-      res.end();
-    }
-  
-    @UseGuards(GoogleAuthGuard)
-    @Get("google/redirect")
-    async googleAuthRedirect(@Req() req: Request & { cookies: any }, @Res() res: Response) {
-      const { accessToken } = await this.authService.googleLogin(req);
-      res.cookie("access_token", accessToken, {
-        httpOnly: true, // ควรเป็น true เพื่อป้องกัน XSS
-        secure: process.env.NODE_ENV === "production", // ควรเป็น true ถ้าใช้ HTTPS
-        domain: "suture-bot.it.kmitl.ac.th", // ไม่ต้องมีจุดนำหน้า
-        sameSite: "none",
-      });
-      const redirectUrl = req.cookies?.["redirect_after_login"] || "/";
-      res.clearCookie("redirect_after_login");
-  
-      const fullRedirectUrl = `${process.env.REACT_APP_API_URL}${redirectUrl}`;  
-      return res.redirect(fullRedirectUrl);
-    }
+  @UseGuards(GoogleAuthGuard)
+  @Get("google/login")
+  async googleAuth(@Req() req: Request & { query: any }, @Res() res: Response) {
+    const redirectUrl = (req.query["redirect"] as string) || "/";
+    res.cookie("redirect_after_login", redirectUrl, {
+      httpOnly: true, // ควรเป็น true เพื่อป้องกัน XSS
+      secure: false, 
+      domain: "suture-bot.it.kmitl.ac.th", // ไม่ต้องมีจุดนำหน้า
+      sameSite: "none",
+      maxAge: 1000 * 60 * 10, // อายุ 10 นาที
+    });
+
+    res.end();
+  }
+
+  @UseGuards(GoogleAuthGuard)
+  @Get("google/redirect")
+  async googleAuthRedirect(@Req() req: Request & { cookies: any }, @Res() res: Response) {
+    const { accessToken } = await this.authService.googleLogin(req);
+    res.cookie("access_token", accessToken, {
+      httpOnly: true, // ควรเป็น true เพื่อป้องกัน XSS
+      secure: false, 
+      domain: "suture-bot.it.kmitl.ac.th", // ไม่ต้องมีจุดนำหน้า
+      sameSite: "none",
+    });
+    const redirectUrl = req.cookies?.["redirect_after_login"] || "/";
+    res.clearCookie("redirect_after_login");
+
+    const fullRedirectUrl = `${process.env.REACT_APP_API_URL}${redirectUrl}`;
+    return res.redirect(fullRedirectUrl);
+  }
   @Get('logout')
   async logout(@Request() req, @Res() res: Response) {
-    res.clearCookie('access_token', {  
+    res.clearCookie('access_token', {
       httpOnly: true, // ควรเป็น true เพื่อป้องกัน XSS
-        secure: process.env.NODE_ENV === "production", // ควรเป็น true ถ้าใช้ HTTPS
-        domain: "suture-bot.it.kmitl.ac.th", // ไม่ต้องมีจุดนำหน้า
-        sameSite: "none",
+      secure: false, 
+      domain: "suture-bot.it.kmitl.ac.th", // ไม่ต้องมีจุดนำหน้า
+      sameSite: "none",
     });
-  
+
     res.status(200).json({ message: "Successfully logged out" });
   }
   @Get('status')
