ce97c97aeee4d044ce5bd66ba304f1cfb6dfd4ba
KittinanPog <64070007@kmitl.ac.th>
2025-03-06 14:59:37 +0700

    Update UpdateAiPage.tsx

--- ReactFrontend/src/pages/Admin/UpdateAiPage.tsx
+++ ReactFrontend/src/pages/Admin/UpdateAiPage.tsx
@@ -44,8 +44,6 @@ const UpdateAiPage: React.FC = () => {
 
   const { aiModelData, isLoadingAiModel } = useAiData();
   const { updateAiModel, deleteAiModel } = useAiModelMutation();
-   const [isPredicting, setIsPredicting] = useState(false);
-
   const [isPredicting, setIsPredicting] = useState(false);
 
   const {
